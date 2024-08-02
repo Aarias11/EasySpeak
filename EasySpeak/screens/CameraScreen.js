2a0ec2378b0ed9d4f90cd2fea7c4902faea85206
Alan Arias <alanarias@Alans-Air.fios-router.home>
2024-08-02 17:52:27 -0400

    Updated Camera and Homescreen to translate

--- EasySpeak/screens/CameraScreen.js
+++ EasySpeak/screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator, Button, ImageBackground, Animated, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator, Button, ImageBackground, Animated, ScrollView, TextInput } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -13,9 +13,55 @@ const CameraScreen = ({ navigation }) => {
   const [translatedText, setTranslatedText] = useState('');
   const [fromLanguage, setFromLanguage] = useState('en'); // Default "from" language is English
   const [toLanguage, setToLanguage] = useState('es'); // Default "to" language is Spanish
+  const [fromLanguageName, setFromLanguageName] = useState('English');
+  const [toLanguageName, setToLanguageName] = useState('Spanish');
+  const [languages, setLanguages] = useState([]);
+  const [filteredLanguages, setFilteredLanguages] = useState([]);
+  const [showFromDropdown, setShowFromDropdown] = useState(false);
+  const [showToDropdown, setShowToDropdown] = useState(false);
+  const [searchFromLanguage, setSearchFromLanguage] = useState('');
+  const [searchToLanguage, setSearchToLanguage] = useState('');
   const translationSlideAnim = useRef(new Animated.Value(0)).current;
 
   const apiKey = 'AIzaSyCGvCBIX2RNeihtAUD-EcGxXJApmFdESzk'; // Replace with your Google Cloud API key
+
+  useEffect(() => {
+    fetchLanguages();
+  }, []);
+
+  useEffect(() => {
+    setFilteredLanguages(languages);
+  }, [languages]);
+
+  const fetchLanguages = async () => {
+    const url = `https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}&target=en`;
+    try {
+      const response = await axios.get(url);
+      setLanguages(response.data.data.languages);
+    } catch (error) {
+      console.error('Error fetching languages:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  const handleSearchFromLanguage = (text) => {
+    setSearchFromLanguage(text);
+    if (text === '') {
+      setFilteredLanguages(languages);
+    } else {
+      const filtered = languages.filter((lang) => lang.name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredLanguages(filtered);
+    }
+  };
+
+  const handleSearchToLanguage = (text) => {
+    setSearchToLanguage(text);
+    if (text === '') {
+      setFilteredLanguages(languages);
+    } else {
+      const filtered = languages.filter((lang) => lang.name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredLanguages(filtered);
+    }
+  };
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -111,12 +157,77 @@ const CameraScreen = ({ navigation }) => {
     }).start();
   };
 
+  const getLanguageName = (code) => {
+    const language = languages.find(lang => lang.language === code);
+    return language ? language.name : 'Unknown';
+  };
+
   return (
     <ImageBackground
       source={require("../../EasySpeak/assets/background/background.png")}
       style={styles.backgroundImage}
     >
       <View style={styles.container}>
+        <View style={styles.languagesTopContainer}>
+          <TouchableOpacity
+            style={styles.translateTopLanguageButton}
+            onPress={() => setShowFromDropdown(!showFromDropdown)}
+          >
+            <Text style={styles.languageTopButtonText}>{getLanguageName(fromLanguage)}</Text>
+            <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
+          </TouchableOpacity>
+          {showFromDropdown && (
+            <View style={styles.dropdownContainer}>
+              <View style={styles.searchContainer}>
+                <MaterialIcons name="search" size={20} color="#ccc" />
+                <TextInput
+                  style={styles.searchInput}
+                  placeholder="Search language"
+                  placeholderTextColor="#ecf0ef"
+                  value={searchFromLanguage}
+                  onChangeText={handleSearchFromLanguage}
+                />
+              </View>
+              <ScrollView style={styles.dropdown}>
+                {filteredLanguages.map(item => (
+                  <TouchableOpacity key={item.language} onPress={() => { setFromLanguage(item.language); setShowFromDropdown(false); }}>
+                    <Text style={styles.dropdownItem}>{item.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          )}
+          <MaterialIcons name="swap-horiz" size={30} color="white" />
+          <TouchableOpacity
+            style={styles.translateTopLanguageButton}
+            onPress={() => setShowToDropdown(!showToDropdown)}
+          >
+            <Text style={styles.languageTopButtonText}>{getLanguageName(toLanguage)}</Text>
+            <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
+          </TouchableOpacity>
+          {showToDropdown && (
+            <View style={styles.dropdownContainer}>
+              <View style={styles.searchContainer}>
+                <MaterialIcons name="search" size={20} color="#ecf0ef" />
+                <TextInput
+                  style={styles.searchInput}
+                  placeholder="Search language"
+                  placeholderTextColor="#ecf0ef"
+
+                  value={searchToLanguage}
+                  onChangeText={handleSearchToLanguage}
+                />
+              </View>
+              <ScrollView style={styles.dropdown}>
+                {filteredLanguages.map(item => (
+                  <TouchableOpacity key={item.language} onPress={() => { setToLanguage(item.language); setShowToDropdown(false); }}>
+                    <Text style={styles.dropdownItem}>{item.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          )}
+        </View>
         <CameraView
           style={styles.camera}
           type={facing}
@@ -144,24 +255,6 @@ const CameraScreen = ({ navigation }) => {
               },
             ],
           }]}>
-            {/* Languages Container */}
-            <View style={styles.languagesContainer}>
-              <TouchableOpacity
-                style={styles.translateLanguageButton}
-                onPress={() => setFromLanguage(fromLanguage === 'en' ? 'es' : 'en')}
-              >
-                <Text style={styles.languageButtonText}>{fromLanguage === 'en' ? 'English' : 'Spanish'} </Text>
-                <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
-              </TouchableOpacity>
-              <MaterialIcons name="swap-horiz" size={30} color="white" />
-              <TouchableOpacity
-                style={styles.translateLanguageButton}
-                onPress={() => setToLanguage(toLanguage === 'es' ? 'en' : 'es')}
-              >
-                <Text style={styles.languageButtonText}>{toLanguage === 'es' ? 'Spanish' : 'English'}</Text>
-                <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
-              </TouchableOpacity>
-            </View>
             <Text style={styles.translationTitle}>Translation:</Text>
             <ScrollView>
               <Text style={styles.translatedText}>{translatedText}</Text>
@@ -180,7 +273,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    marginTop: 50,
     alignItems: "center",
   },
   camera: {
@@ -239,7 +331,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(10, 10, 10, 0.65)',
     margin: 10,
     position: 'absolute',
-    top: 10,
+    top: 150,
     zIndex: 0,
   },
   translationTitle: {
@@ -268,16 +360,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#007AFF',
     borderRadius: 50,
   },
-
   languagesContainer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     marginTop: 10,
     marginBottom: 10,
-    gap: 10
+    gap: 10,
+    zIndex: 20, // Ensure the dropdown stays in front of the translation container
+  },
+  languagesTopContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    gap: 10,
+    zIndex: 20, // Ensure the dropdown stays in front of the translation container
+    position: 'absolute',
+    top: 100,
+    width: '100%',
+    paddingHorizontal: 10,
   },
   translateLanguageButton: {
+    backgroundColor: "#297386",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginHorizontal: 5, // Add spacing between buttons
+    flexDirection: 'row',
+    justifyContent: "center",
+    gap: 5,
+    alignItems: 'center'
+  },
+  translateTopLanguageButton: {
     backgroundColor: "#297386",
     paddingVertical: 10,
     paddingHorizontal: 20,
@@ -291,6 +407,43 @@ const styles = StyleSheet.create({
   languageButtonText: {
     color: "white",
     fontSize: 16,
+  },
+  languageTopButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  dropdownContainer: {
+    maxHeight: 300, // Limit the height of the dropdown
+    width: '100%',
+    position: 'absolute',
+    top: 50,
+    zIndex: 30,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#0a0f0e',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    
+  },
+  searchInput: {
+    marginLeft: 10,
+    flex: 1,
+    padding: 15,
+    color: '#ecf0ef'
+
+  },
+  dropdown: {
+    backgroundColor: '#0a0f0e',
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15
+  },
+  dropdownItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    color: '#ecf0ef'
   },
 });
 
--- EasySpeak/screens/HomeScreen.js
+++ EasySpeak/screens/HomeScreen.js
@@ -1,166 +1,340 @@
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, ImageBackground, TextInput } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons'; // 
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, ImageBackground, TextInput, ScrollView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
-
+import axios from 'axios';
 
 const HomeScreen = ({ navigation }) => {
-    return (
-        <ImageBackground
-            source={require('../../EasySpeak/assets/background/background.png')} 
-            style={styles.backgroundImage}
-        >
-            {/* Container */}
-            <View style={styles.container}>
-            {/* Avatar and Settings Container */}
-                <View style={styles.avatarAndSettingsContainer}>
-                {/* Avatar Container */}
-                    <View style={styles.avatarContainer}>
-                        <TouchableOpacity onPress={() => navigation.navigate('Account')}>
-                            <MaterialIcons name="circle" size={60} color="white" />
-                        </TouchableOpacity>
-                    </View>
-                    {/* Settings Icon Button */}
-                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-                        <MaterialIcons name="settings" size={30} color="white" />
-                    </TouchableOpacity>
-                </View>
-                {/* App Name Container */}
-                <View style={styles.appNameContainer}>
-                    <Text style={styles.appNameHeader}>EasySpeak</Text>
-                </View>
-                {/* Languages Container */}
-                <View style={styles.languagesContainer}>
-                <TouchableOpacity style={styles.translateLanguageButton}>
-            <Text style={styles.languageButtonText}>English </Text>
+  const [fromLanguage, setFromLanguage] = useState('en'); // Default "from" language is English
+  const [toLanguage, setToLanguage] = useState('es'); // Default "to" language is Spanish
+  const [languages, setLanguages] = useState([]);
+  const [showFromDropdown, setShowFromDropdown] = useState(false);
+  const [showToDropdown, setShowToDropdown] = useState(false);
+  const [searchFromLanguage, setSearchFromLanguage] = useState('');
+  const [searchToLanguage, setSearchToLanguage] = useState('');
+  const [filteredLanguages, setFilteredLanguages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+  const apiKey = 'AIzaSyCGvCBIX2RNeihtAUD-EcGxXJApmFdESzk'; // Replace with your Google Cloud API key
+
+  useEffect(() => {
+    fetchLanguages();
+  }, []);
+
+  useEffect(() => {
+    setFilteredLanguages(languages);
+  }, [languages]);
+
+  const fetchLanguages = async () => {
+    const url = `https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}&target=en`;
+    try {
+      const response = await axios.get(url);
+      setLanguages(response.data.data.languages);
+    } catch (error) {
+      console.error('Error fetching languages:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  const handleSearchFromLanguage = (text) => {
+    setSearchFromLanguage(text);
+    if (text === '') {
+      setFilteredLanguages(languages);
+    } else {
+      const filtered = languages.filter((lang) => lang.name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredLanguages(filtered);
+    }
+  };
+
+  const handleSearchToLanguage = (text) => {
+    setSearchToLanguage(text);
+    if (text === '') {
+      setFilteredLanguages(languages);
+    } else {
+      const filtered = languages.filter((lang) => lang.name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredLanguages(filtered);
+    }
+  };
+
+  const getLanguageName = (code) => {
+    const language = languages.find(lang => lang.language === code);
+    return language ? language.name : 'Unknown';
+  };
+
+  const translateText = async () => {
+    const translateUrl = `https://translation.googleapis.com/language/translate/v2`;
+
+    const translatePayload = {
+      q: inputText,
+      target: toLanguage,
+      source: fromLanguage,
+      key: apiKey,
+    };
+
+    try {
+      const response = await axios.post(translateUrl, null, { params: translatePayload });
+      const translation = response.data.data.translations[0].translatedText;
+      setTranslatedText(translation);
+    } catch (error) {
+      console.error('Error translating text:', error.response ? error.response.data : error.message);
+    }
+  };
+
+  return (
+    <ImageBackground
+      source={require('../../EasySpeak/assets/background/background.png')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <View style={styles.avatarAndSettingsContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('Account')}>
+            <MaterialIcons name="circle" size={60} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+            <MaterialIcons name="settings" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.appNameContainer}>
+          <Text style={styles.appNameHeader}>EasySpeak</Text>
+        </View>
+        <View style={styles.languagesContainer}>
+          <TouchableOpacity
+            style={styles.translateLanguageButton}
+            onPress={() => setShowFromDropdown(!showFromDropdown)}
+          >
+            <Text style={styles.languageButtonText}>{getLanguageName(fromLanguage)}</Text>
             <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
           </TouchableOpacity>
-          <MaterialIcons name="swap-horiz" size={30} color="white" />
-          <TouchableOpacity style={styles.translateLanguageButton}>
-            <Text style={styles.languageButtonText}>Spanish</Text>
-            <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
-          </TouchableOpacity>
-                </View>
-                {/* Translation Box */}
-                <View style={styles.translationBox}>
-                    {/* <TouchableOpacity style={styles.translateLanguageButton}>
-                        <Text style={styles.translationFromButtonText}>English</Text>
-                    </TouchableOpacity> */}
-                    {/* Blur */}
-                    <BlurView intensity={15} style={styles.blur}>
-                        <TextInput
-                        style={styles.inputTranslation}
-                        placeholder="Type or Push Mic to Translate"
-                        placeholderTextColor="#A7CCD6" />
-                    </BlurView>
-                </View>
+          {showFromDropdown && (
+            <View style={styles.dropdownContainer}>
+              <View style={styles.searchContainer}>
+                <MaterialIcons name="search" size={20} color="#ccc" />
+                <TextInput
+                  style={styles.searchInput}
+                  placeholder="Search language"
+                  placeholderTextColor="#ccc"
+                  value={searchFromLanguage}
+                  onChangeText={handleSearchFromLanguage}
+                />
+              </View>
+              <ScrollView style={styles.dropdown}>
+                {filteredLanguages.map(item => (
+                  <TouchableOpacity key={item.language} onPress={() => { setFromLanguage(item.language); setShowFromDropdown(false); }}>
+                    <Text style={styles.dropdownItem}>{item.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
             </View>
-        </ImageBackground>
-    );
-}
+          )}
+          <MaterialIcons name="swap-horiz" size={30} color="white" />
+          <TouchableOpacity
+            style={styles.translateLanguageButton}
+            onPress={() => setShowToDropdown(!showToDropdown)}
+          >
+            <Text style={styles.languageButtonText}>{getLanguageName(toLanguage)}</Text>
+            <MaterialIcons name="keyboard-arrow-down" color={'white'} size={15} />
+          </TouchableOpacity>
+          {showToDropdown && (
+            <View style={styles.dropdownContainer}>
+              <View style={styles.searchContainer}>
+                <MaterialIcons name="search" size={20} color="#ccc" />
+                <TextInput
+                  style={styles.searchInput}
+                  placeholder="Search language"
+                  placeholderTextColor="#ccc"
+                  value={searchToLanguage}
+                  onChangeText={handleSearchToLanguage}
+                />
+              </View>
+              <ScrollView style={styles.dropdown}>
+                {filteredLanguages.map(item => (
+                  <TouchableOpacity key={item.language} onPress={() => { setToLanguage(item.language); setShowToDropdown(false); }}>
+                    <Text style={styles.dropdownItem}>{item.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          )}
+        </View>
+        <View style={styles.translationBox}>
+          <BlurView intensity={15} style={styles.blur}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputTranslation}
+                placeholder="Type or Push Mic to Translate"
+                placeholderTextColor="#A7CCD6"
+                value={inputText}
+                onChangeText={setInputText}
+              />
+              <TouchableOpacity onPress={translateText} style={styles.translateButton}>
+                <Text style={styles.translateButtonText}>Translate</Text>
+              </TouchableOpacity>
+            </View>
+            {translatedText ? (
+              <View style={styles.translatedTextContainer}>
+                <Text style={styles.translatedText}>{translatedText}</Text>
+              </View>
+            ) : null}
+          </BlurView>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover',
-    },
-    container: {
-        flex: 1,
-        marginTop: 50
-        // Optional overlay
-    },
-    avatarAndSettingsContainer: {
-        width: '100%',
-        height: 100,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 17,
-    },
-    // avatarContainer: {
-    //     width: 60,
-    //     height: 60,
-    //     borderWidth: 1,
-    //     borderColor: '#F1F2F2',
-    //     borderRadius: 30,
-    // },
-    appNameContainer: {
-        width: '100%',
-        alignItems: 'center',
-        bottom: 20
-    },
-    appNameHeader: {
-        color: 'white',
-        fontSize: 32,
-        fontWeight: '700',
-    },
-    translationBox: {
-        width: '100%',
-        height: 590,
-        borderWidth: 0.2,
-        borderColor: '#F1F2F2',
-        borderRadius: 25,
-        marginTop: 12,
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        zIndex: 50,
-        opacity: 0.4,
-        position: 'relative',
-        
-    },
-    blur: {
-        width: '100%',
-        height: '100%',
-        borderRadius: 25,
-        overflow: 'hidden',
-        
-       
-    },
-    inputTranslation: {
-        color: '#A7CCD6',
-        fontSize: 20,
-        top: 40,
-        left: 30,
-        fontWeight: '600'
-    
-    },
-    translateLanguageButton: {
-        width: 100,
-        height: 40,
-        borderWidth: 1,
-        borderRadius: 10,
-        backgroundColor: '#2CB5DA',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        top: -20,
-        left: 20,
-        zIndex: 10
-    },
-    translationFromButtonText: {
-        color: '#F1F2F2',
-    },
-    languagesContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 10,
-        gap: 10
-      },
-      translateLanguageButton: {
-        backgroundColor: "#297386",
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        marginHorizontal: 5, // Add spacing between buttons
-        flexDirection: 'row',
-        justifyContent: "center",
-        gap: 5,
-        alignItems: 'center'
-      },
-      languageButtonText: {
-        color: "white",
-        fontSize: 16,
-      },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  avatarAndSettingsContainer: {
+    width: '100%',
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 17,
+  },
+  appNameContainer: {
+    width: '100%',
+    alignItems: 'center',
+    bottom: 20,
+  },
+  appNameHeader: {
+    color: 'white',
+    fontSize: 32,
+    fontWeight: '700',
+  },
+  translationBox: {
+    width: '100%',
+    height: 590,
+    borderWidth: 0.2,
+    borderColor: 'rgba(60, 60, 60, 0.4)',
+    borderRadius: 25,
+    marginTop: 12,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    position: 'relative',
+    zIndex: 1,
+  },
+  blur: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 25,
+    overflow: 'hidden',
+    padding: 20,
+  },
+  inputContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputTranslation: {
+    color: '#A7CCD6',
+    fontSize: 20,
+    fontWeight: '600',
+    width: '100%',
+  },
+  translateButton: {
+    marginTop: 20,
+    backgroundColor: '#2CB5DA',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  translateButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  translatedTextContainer: {
+    marginTop: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    padding: 10,
+    borderRadius: 10,
+  },
+  translatedText: {
+    color: '#A7CCD6',
+    fontSize: 18,
+  },
+  translateLanguageButton: {
+    backgroundColor: "#297386",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginHorizontal: 5, // Add spacing between buttons
+    flexDirection: 'row',
+    justifyContent: "center",
+    gap: 5,
+    alignItems: 'center',
+    zIndex: 30, // Ensure the button stays in front
+  },
+  languageButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  dropdownContainer: {
+    maxHeight: 300, // Limit the height of the dropdown
+    width: '90%',
+    position: 'absolute',
+    top: 50,
+    zIndex: 30,
+    backgroundColor: '#0a0f0e', // Ensure it stays in front
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#0a0f0e',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  searchInput: {
+    marginLeft: 10,
+    flex: 1,
+    padding: 10,
+    color: '#ecf0ef'
+  },
+  dropdown: {
+    backgroundColor: '#0a0f0e',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  dropdownItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    color: '#ecf0ef'
+  },
+  languagesContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    gap: 10,
+    zIndex: 20,
+  },
+  languagesTopContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    gap: 10,
+    zIndex: 20,
+    position: 'absolute',
+    top: 100,
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  languageTopButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
 });
 
 export default HomeScreen;
